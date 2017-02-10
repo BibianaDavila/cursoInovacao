a18c4da21208c529af0fe26aabb3bd2706272c49
BibianaDavila <davila.bibiana@gmail.com>
2017-02-10 16:18:16 -0200

    Curso de inovação para o site da OBEC, primeiro commit

---
+++ index.php
@@ -0,0 +1,483 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<title>Inovação e Empreendedorismo Em Indústrias Criativas</title>
+
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="js/bootstrap.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+		<div class="site-wrapper">
+
+			<!--========== section 1 ===========-->
+			<section>
+				<article id="home">
+
+					<!--=== home! ===-->
+					<div class="container-inner">
+					
+						<!--=== logo ===-->
+						<div class="row">
+							<div class="col-sm-3 col-xs-4 col-xxs-4 logo">
+								<img src="image/logo.png">
+							</div>
+						</div>
+
+						<!--=== título ===-->
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="title-wrapper">
+									<div class="title">
+										<div class="text-right"><span class="bigger">INOVAÇÃO</span> <span class="e">E</span></div>
+										<div><span class="bigger">EMPREENDEDORISMO</span></div>
+										<div><div class="em">EM</div><span class="bigger">INDÚSTRIAS </span></div>
+										<div><span class="bigger criativas">CRIATIVAS</span></div>
+									</div>
+								</div>
+								<div class="line-height"></div>
+							</div>
+						</div>
+
+						<!--=== info curso ===-->
+						<div class="row">
+
+							<!--=== descricao ===-->
+							<div class="col-md-4 col-sm-5 col-sm-offset-0 col-xs-12">
+								<div class="desc-wrapper">
+									
+									<p>Destacar a importância do olhar econômico para criatividade, propagando-o para além da academia.</p>
+									
+									<p>Capacitar empreendedores quanto a inputs da teoria da economia criativa, relacionando-os a casos reais e sublinhando o papel da inovação para o sucesso de projetos. Contextualizar o empreendedor quanto a seu papel no território, apresentando, por um lado, background de atuação, e por outro, possibilidades e oportunidades para inovação.</p>
+
+								</div>
+							</div>
+
+							<!--=== inscricoes ===-->
+							<div class="col-md-4 col-sm-3 col-xs-12">
+								<div class="insc-wrapper">
+									<p><a href="">Edital de Seleção ></a></p>
+									<p><a href="https://form.jotformz.com/70385085491663" target="_blank">Inscrições ></a></p>
+								</div>
+							</div>
+
+							<!--=== horários ===-->
+							<div class="col-md-4 col-sm-4 col-sm-offset-0 col-xs-12">
+								<div class="row">
+
+									<!-- publico alvo -->
+									<div class="col-sm-6 col-xs-5 col-sm-offset-0">
+										<div class="row publico-wrapper text-center">
+											<div class="content">PEQUENOS EMPREENDEDORES<br/>CULTURAIS/CRIATIVOS</div>
+											<b>PÚBLICO ALVO</b>
+										</div>
+									</div>
+
+									<!-- horarios -->
+									<div class="col-xs-3 visible-xs">
+										<div class="horarios-wrapper text-center">
+											
+											<div class="tempo mobile">
+												<div class="horario"><div class="content horas">6</div><div class="content semanas">SEMANAS</div><b>DE DURAÇÃO</b></div>
+											</div>
+
+										</div>
+									</div>
+
+									<div class="col-xs-4 col-sm-5">
+										<div class="horarios-wrapper text-center">
+
+											<div class="tempo text-center">
+												<div class="horario"><div class="content horas">30H</div><b>DE CURSO</b></div>
+											</div>
+
+											<div class="divider hidden-xs"></div>
+											
+											<div class="tempo hidden-xs">
+												<div class="horario"><div class="content horas">6</div><div class="content semanas">SEMANAS</div><b>DE DURAÇÃO</b></div>
+											</div>
+
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</article>
+			</section>
+
+			<!--========== section 2 ===========-->
+			<section class="invert">
+				<article id="div-1">
+
+					<!--=== primeiro modulo ===-->
+					<div class="container-inner">
+					
+						<div class="row">
+
+							<!-- quadro horários -->
+							<div class="quadro-wrapper after">
+
+								<!-- imagem quadro horas -->
+								<div class="quadro-horas-img">
+									<div class="quadro-horas-txt">
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+									</div>
+								</div>
+
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-5 col-md-5 col-sm-3 col-xs-12">
+								<div class="datas-wrapper">
+									<span class="bold">15</span> E <span class="bold">16</span><br/>
+									DE MARÇO<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>
+
+							<!-- descrição -->
+							<div class="col-lg-6 col-md-6 col-sm-7 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper">
+									
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO I <div class="bold">ECONOMIA DA CRIATIVIDADE</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: O primeiro módulo tem como principal objetivo a apresentação de conceitos básicos como Economia Criativa, Economia da Cultura, Valor Simbólico e Setores Culturais e Criativos (SCC).</p>
+
+									<p>Tal introdução será baseada em metodologias internacionais. Além disso, será traçado um panorama a respeito das tendências recentes da Economia Criativa, organizado em torno de eixos como: novos mercados; setores em expansão; novos modelos de negócios; novas formas de trabalho; dentre outros. </p>
+
+									<p>Ao fim do módulo, espera-se elucidar a conexão entre tais conceitos e os desafios atuais do empreendedor criativo/cultural.</p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: Prof. Dr. Marcelo Milan (UFRGS) e <br/>Prof. Dr. Leandro Valiati (UFRGS)
+									</div>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</article>
+			</section>
+
+			<!--========== section 3 ===========-->
+			<section class="after-invert">
+				<article id="div-2">
+
+					<!--=== segundo modulo ===-->
+					<div class="container-inner">
+						<div class="row">
+
+							<!-- descrição -->
+							<div class="col-lg-6 col-md-7 col-sm-8 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper invert">
+									
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO II
+										<div class="bold">EMPREENDEDORISMO CRIATIVO E NOVAS TECNOLOGIAS: O PAPEL DA DIGITALIZAÇÃO</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: O segundo módulo tratará da relação entre inovação tecnológica e empreendedorismo criativo a partir de dois níveis de análise. </p>
+
+									<p>No primeiro, atenta-se para estrutura produtiva global, conectando a emergência da digitalização com um novo paradigma tecnológico, no qual os Setores Culturais e Criativos (SCC) são protagonistas.</p>
+
+									<p>No segundo nível análise, enfoca-se a relação entre digitalização e novos modelos de negócios, expondo cases de sucesso e ferramentas digitais úteis para empreendedor criativo/cultural. </p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: Prof. Dr. François Moreau (FRA) e<br/>representante games (Virgo Games)
+									</div>
+
+								</div>
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-6 col-md-5 col-sm-4 col-xs-12">
+								<div class="datas-wrapper invert">
+									<span class="bold">22</span> E <span class="bold">23</span><br/>
+									DE MARÇO<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>	
+
+							<!-- quadro horários -->
+							<div class="quadro-wrapper invert">
+
+								<!-- imagem quadro horas -->
+								<div class="quadro-horas-img">
+									<div class="quadro-horas-txt">
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+										<br/>
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+									</div>
+								</div>
+							</div>						
+
+						</div>
+					</div>
+
+					<!--=== terceiro modulo ===-->
+					<div class="container-inner">
+						<div class="row">
+
+							<!-- descrição -->
+							<div class="col-lg-6 col-md-7 col-sm-8 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper after-invert">
+
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO III<div class="bold">O DIFERENCIAL DO EMPREENDEDOR CRIATIVO: INOVAÇÃO EM PROCESSOS E NA RELAÇÃO COM O TERRITÓRIO</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: O terceiro módulo apresentará o conceito de inovação em processos, localizando-o no ambiente enfrentado por empreendimentos criativos.</p>
+									
+									<p>Além disso, atenta-se para a articulação desses empreendimentos entre si e com seus respectivos territórios, modormando territórios criativos. </p>
+
+									<p> Nesse sentido, serão expostos exemplos nacionais e internacionais de territórios criativos, bem como instrumentos de políticas públicas, como a criação de incubadoras criativas, a serem utilizadas para o fomento de tais iniciativas. </p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: Prof. Dr. Claudio Muller (UFRGS), Ivana Beltrão (Rio Criativo) <br/>e Marcos Oliveira (Porto Digital)
+									</div>
+
+								</div>
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-6 col-md-5 col-sm-4 col-xx-12">
+								<div class="datas-wrapper after-invert">
+									<span class="bold">29</span> E <span class="bold">30</span><br/>
+									DE MARÇO<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>	
+
+						</div>
+					</div>
+
+				</article>
+			</section>
+
+			<!--========== section 4 ===========-->
+			<section class="invert">
+				<article id="div-3">
+
+					<!--=== quarto modulo ===-->
+					<div class="container-inner">
+						<div class="row">
+
+							<!--=== quadro horários ===-->
+							<div class="quadro-wrapper after">
+
+								<!-- imagem quadro horas -->
+								<div class="quadro-horas-img">
+									<div class="quadro-horas-txt">
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+									</div>
+								</div>
+
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-6 col-md-4 col-sm-3 col-xs-12">
+								<div class="datas-wrapper">
+									<span class="bold">05</span> E <span class="bold">06</span><br/>
+									DE ABRIL<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>		
+
+							<!-- descrição -->
+							<div class="col-lg-5 col-md-6 col-sm-7 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper">
+									
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO IV <div class="bold">INOVAÇÃO QUE VEM DE FORA: O CASO DA ESPANHA</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: O quarto módulo consiste na apresentação de experiências bem-sucedidas da Espanha na compreensão da Economia Criativa e no impulso a novos modelos de negócios.</p>
+
+									<p>Com ênfase no desenvolvimento local e na cultura como motor da inovação, serão apresentados cases de sucesso e instrumentos para a avaliação do impacto econômico e social desses empreendimentos.</p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: Prof. Dr. Pau Rausel Köster (ESP)
+									</div>
+
+								</div>
+							</div>						
+
+						</div>
+					</div>
+
+				</article>
+			</section>
+
+			<!--========== section 5 ===========-->
+			<section class="after-invert">
+				<article id="div-4">
+
+					<!--=== quinto modulo ===-->
+					<div class="container-inner">
+						<div class="row">
+
+							<!-- descrição -->
+							<div class="col-lg-6 col-md-7 col-sm-8 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper invert">
+									
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO V <div class="bold">INTERNACIONALIZAÇÃO EM INDÚSTRIAS CRIATIVAS</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: No quinto módulo, o curso volta-se aos principais desafios enfrentados por empreendedores criativos. Assim, discute-se os aspectos da legislação que mais afetam tais empreendimentos, bem como o tema dos direitos de propriedade intelectual, fundamentais na mensuração do valor econômico da cultura e da criatividade.</p>
+
+									<p>Além disso, o módulo tratará das especificidades do processo de internacionalização por parte de Setores Culturais e Criativos (SCC), sublinhando aspectos como principais mercados ao redor do mundo, desafios organizacionais típicos, dentre outros. </p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: Claudio Vasconcelos <br/>e Criatiano Braga (APEX).
+									</div>
+
+								</div>
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-6 col-md-5 col-sm-4 col-xxs-3  col-sxxs-4">
+								<div class="datas-wrapper invert">
+									<span class="bold">11</span> E <span class="bold">12</span><br/>
+									DE ABRIL<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>	
+
+							<!--=== quadro horários ===-->
+							<div class="quadro-wrapper invert custom">
+
+								<!-- imagem quadro horas -->
+								<div class="quadro-horas-img">
+									<div class="quadro-horas-txt">
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+										<br/>
+										<div class="horas">5H</div>
+										<div class="legenda">DE DURAÇÃO</div>
+									</div>
+								</div>
+							</div>						
+
+						</div>
+					</div>
+
+					<!--=== sexto modulo ===-->
+					<div class="container-inner">
+						<div class="row">
+
+							<!-- descrição -->
+							<div class="col-lg-6 col-md-7 col-sm-8 col-sm-offset-0 col-xs-12">
+								<div class="mod-wrapper after-invert">
+									
+
+									<div class="divider visible-xs"></div>
+
+									<!-- título! -->
+									<div class="mod-titulo">
+										MÓDULO VI <div class="bold">POLÍTICAS CULTURAIS E RECURSOS PÚBLICOS</div>
+									</div>
+
+									<div class="divider hidden-xs"></div>
+									<br class="visible-xs"/>
+
+									<p>Conteúdo: Por fim, o último módulo tratará das políticas de fomento à cultural no Brasil, expondo seu funcionamento e os recursos disponibilizados em nível municipal e federal.</p>
+
+									<p>O principal objetivo desse módulo final é preparar empreendedores interessados para obter recursos públicos, empregando-os na expansão dos empreendimentos criativos e na gestação de novos modelos de negócios.</p>
+
+									<br/>
+
+									<div class="light text-right">
+										Instrutores: A definir.
+									</div>
+
+								</div>
+							</div>
+
+							<!-- datas -->
+							<div class="col-lg-6 col-md-5 col-sm-4 col-xs-12">
+								<div class="datas-wrapper after-invert">
+									<span class="bold">18</span> E <span class="bold">19</span><br/>
+									DE ABRIL<br class="hidden-xs"/>
+									DE 2017<br/>
+								</div>
+							</div>	
+
+						</div>
+					</div>
+					
+				</article>
+			</section>
+
+			<!--========== realizadores ===========-->
+			<div class="container-inner">
+				<div class="realizador-images">
+					<img src="image/realizadores/ufrgs.png">
+					<img src="image/realizadores/cegov.png">
+					<img src="image/realizadores/neccult.png">
+					<img src="image/realizadores/obec.png" id="obec">							
+					<img src="image/realizadores/catavento.png">		
+					<img src="image/realizadores/brasil.png">							
+				</div>
+			</div>
+
+		</div>
+	</body>
+</html>
